e43ce105f4d66aa2855ad3f6fe3837d7be93e2f6
hrbrmstr <bob@rud.is>
2022-10-07 06:06:37 -0400

    chore: forgot a q

--- Formula/ohq2quarto.rb
+++ Formula/ohq2quarto.rb
@@ -1,5 +1,5 @@
-class Oh2quarto < Formula
-  desc "oh2quarto — Save an Observable HQ Notebook to a Quarto project."
+class Ohq2quarto < Formula
+  desc "ohq2quarto — Save an Observable HQ Notebook to a Quarto project."
   homepage "https://github.com/hrbrmstr/ohq2quarto"
   version "0.2.0"
 
@@ -8,13 +8,13 @@ class Oh2quarto < Formula
     sha256 "2855f89d70d2241c50f1a29c54e5b0a10f074d1e002e5c3e336e780a021138c4"
   end
 
-  conflicts_with "oh2quarto"
+  conflicts_with "ohq2quarto"
 
   def install
-    bin.install "oh2quarto"
+    bin.install "ohq2quarto"
   end
 
   test do
-    system "#{bin}/oh2quarto --version"
+    system "#{bin}/ohq2quarto --version"
   end
 end
