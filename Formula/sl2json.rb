81c67ac8d8b39974f2ca3a68aa32da07c2129ad3
hrbrmstr <bob@rud.is>
2022-10-07 06:22:14 -0400

    chore: add sl2json hash

--- Formula/sl2json.rb
+++ Formula/sl2json.rb
@@ -1,11 +1,11 @@
 class Sl2json < Formula
-  desc "sl2json — Command line Apple WeatherKit REST API weather conditions and hourly/daily foreacast utility."
+  desc "sl2json — Standalone program to convert Lowrance SL2 files to ndjson."
   homepage "https://github.com/hrbrmstr/sl2json"
   version "0.1.0"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/hrbrmstr/sl2json/releases/download/v0.1.0/sl2json-macos-universal-signed-0.1.0.zip"
-    sha256 "ec32b2f52060411e18965702a7257f8fad83410d59b35dba30062a03c3867230"
+    sha256 "65dbdd15244eac83a92eb07b400d7f41dbb84576af5a50fec6fe80e2b8be5ca4"
   end
 
   conflicts_with "sl2json"
